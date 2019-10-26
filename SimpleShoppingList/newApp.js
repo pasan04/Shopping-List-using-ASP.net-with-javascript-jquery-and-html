d22ee975e9b67dafa4e3d27baf002e2b9543d232
kanishka04 <57023431+kanishka04@users.noreply.github.com>
2019-10-26 07:08:13 +0530

    Added a modification to the code with app.js

---
+++ SimpleShoppingList/newApp.js
@@ -0,0 +1,76 @@
+var myNinjaApp =angular.module('myNinjaApp',[]);
+
+
+
+myNinjaApp.controller('NinjaController',['$scope',function($scope){
+
+  $scope.removeNinja = function(ninja){
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja,1);
+
+
+  };
+  $scope.addNinja = function(newninja){
+
+
+      $scope.ninjas.push({
+        name: $scope.newninja.name,
+        belt: $scope.newninja.belt,
+        rate: parseInt($scope.newninja.rate),
+        available:true
+      });
+
+      $scope.newninja.name = " ";
+      $scope.newninja.belt = " ";
+      $scope.newninja.rate = " ";
+
+      $window.alert('Hi!! ');
+  };
+
+  $scope.ninjas=[
+    {
+      name :"YOSHI",
+      belt:"green",
+      rate :50,
+      available:false
+
+    },
+    {
+      name :"Pasan",
+      belt:"red",
+      rate :50,
+      available:true
+
+    },
+    {
+      name :"Naveen",
+      belt:"yellow",
+      rate :50,
+      available:true
+
+    },
+    {
+      name :"A.u",
+      belt:"green",
+      rate :50,
+      available:true
+
+    },
+    {
+      name :"Naveen",
+      belt:"green",
+      rate :50,
+      available:true
+
+    },
+    {
+      name :"Charith",
+      belt:"pink",
+      rate :50,
+      available:true
+
+    }
+
+  ];
+
+}]);
